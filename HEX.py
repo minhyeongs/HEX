0408870142a452b277e12c7bcaa92e575ebfeef9
minhyeongs <162958572+minhyeongs@users.noreply.github.com>
2024-04-26 23:05:25 +0900

    Update HEX.py

--- HEX.py
+++ HEX.py
@@ -26,7 +26,7 @@ async def on_message(message):
 
     # 특정 단어가 포함되어 있을 때 랜덤으로 반응
     if '헥스야' in message.content:
-        responses = ['Yes, my kitten?', '포옹<3', 'Hi, my Love Anna', "Remember that. i am your daddy."]
+        responses = ['Yes, my kitten?', '포옹<3', 'Hi, my Love Anna', "Remember that. I Love you, my anna.", "Come.", "Kneel.", "콧추 크다하..."]
         response = random.choice(responses)
         await message.channel.send(response)
 
